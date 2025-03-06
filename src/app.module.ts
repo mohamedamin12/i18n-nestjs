2dcaee5da2ae285db3ff36e73ad3f4ce073edc2b
mohamedamin12 <aboamin909080@gmail.com>
2025-03-06 23:56:55 +0200

    add i18n support with Arabic and English translations, update app module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as path from 'path';
+import { AcceptLanguageResolver, HeaderResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
 
 @Module({
-  imports: [],
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+        new HeaderResolver(['x-lang']),
+      ],
+    }),
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
